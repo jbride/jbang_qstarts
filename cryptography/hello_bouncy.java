454003cd5005060c6fac7fdc9fb285a4c8d07b72
jbride <jbride2001@yahoo.com>
2023-12-12 06:15:09 -0700

    latest

--- cryptography/hello_bouncy.java
+++ cryptography/hello_bouncy.java
@@ -6,16 +6,31 @@
 
 import java.security.MessageDigest;
 import java.security.Security;
+import javax.crypto.Cipher;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 public class hello_bouncy {
 
+    private static final String CRYPTO_POLICY="crypto.policy";
+    private static final String UNLIMITED="unlimited";
+
     public static void main(String[] args) throws Exception {
+
         // adds BC to the end of the precedence list
         Security.addProvider(new BouncyCastleProvider());
         System.out.println(MessageDigest.getInstance("SHA1").getProvider().getName());
         System.out.println(MessageDigest.getInstance("SHA1", "BC").getProvider().getName());
+
+
+        // https://www.baeldung.com/java-bouncy-castle#setup-unlimited-strength-jurisdiction-policy-files
+        // $JAVA_HOME/conf/security/java.security
+        int maxKeySize = Cipher.getMaxAllowedKeyLength("AES"); 
+        System.out.println("initial maxKeySize = "+maxKeySize+" : crypto.policy = "+Security.getProperty(CRYPTO_POLICY));
+        if(!UNLIMITED.equals(Security.getProperty(CRYPTO_POLICY))) {
+            Security.setProperty(CRYPTO_POLICY, UNLIMITED);
+            System.out.println("Unlimited Strength Jurisdiction maxKeySize = "+maxKeySize);
+        }
     }
 
 }
